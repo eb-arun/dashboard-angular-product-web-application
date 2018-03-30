da8726aec863e2dacf2db4ed27a8082e2c37632c
Arun Boopathi <a.boopathi@perficient.com>
2018-03-30 18:04:46 +0530

    Pushed updates

--- src/app/user-data/user-data.component.ts
+++ src/app/user-data/user-data.component.ts
@@ -9,9 +9,9 @@ import { RouterModule, Routes, Router } from '@angular/router';
 interface Post {
   title: string;
   content: string;
-  name:string;
-  oil_history:string;
-  email:string;
+  name: string;
+  oil_history: string;
+  email: string;
 }
 @Component({
   selector: 'app-user-data',
@@ -21,37 +21,36 @@ interface Post {
 })
 
 export class UserDataComponent {
-  userName:string;
-  userFirstName:string;
-  profilePictrue:string;
-  userEmail:any;
+  userName: string;
+  userFirstName: string;
+  profilePictrue: string;
+  userEmail: any;
   hero;
   oilData: any;
   userData: AngularFirestoreCollection<Post>;
-  userDoc:Observable<any>;
-  posts: any;
-  checkData:any;
-  localUser:any;
-  constructor(public authService: AuthService, private db: AngularFirestore, private router:Router) { 
+  userDoc: Observable<any>;
+  checkData: any;
+  localUser: any;
+  constructor(public authService: AuthService, private db: AngularFirestore, private router: Router) {
   }
-  ngOnInit () {
-      this.userData = this.db.collection("users");
-      this.userDoc = this.db.collection("users").doc("arun").snapshotChanges();
-     this.userData.valueChanges().subscribe(res =>{
-         this.checkData = res.find(x=>x.email ==this.userEmail);
-         console.log(res , this.checkData, "db");
-      });
-      console.log(this.posts, "Chek");
-      if (localStorage.getItem("userdata") === null) {
-          this.router.navigate(['/logout']);
-      } else {
+  ngOnInit() {
+
+    if (localStorage.getItem("userdata") === null) {
+      this.router.navigate(['/logout']);
+    } else {
       this.localUser = JSON.parse(localStorage.getItem("userdata"));
       console.log(this.localUser.email, "local");
       this.userFirstName = this.localUser.given_name;
       this.userEmail = this.localUser.email;
-      }
-}
+      this.userData = this.db.collection("users", ref => ref.where("email", '==', this.userEmail));
+      this.userDoc = this.db.collection("users").doc("arun").snapshotChanges();
+      this.userData.valueChanges().subscribe(res => {
+        this.checkData = res[0];
+        console.log(res, this.checkData, "db");
+      });
+    }
+  }
 
- 
+
 
 }
